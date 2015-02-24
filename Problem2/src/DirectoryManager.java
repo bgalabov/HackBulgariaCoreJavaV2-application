147c328037f7618bd46f8225f3507fcc6ff4a307
bgalabov <borislav.galabov@gmail.com>
2015-02-24 17:30:04 +0200

    Commiting the solved problems

---
+++ Problem2/src/DirectoryManager.java
@@ -0,0 +1,86 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class DirectoryManager {
+	private static HashMap<String, String> files=new HashMap<String, String>();
+	
+	/* The idea is we calculate the checksum for each file in the directory
+	 and then we check if there is a value in the HashMap corresponding on the
+	 calculated checksum (the checksum is the key) and if there is, we ignore this file.
+	 If the checksum is unique for the hashmap, we put a record in the hashmap and the file name
+	 in the ArrayList which is returned by the listDuplicatingFiles(dir) method.*/
+	 
+	public static ArrayList<String> listDuplicatingFiles(File dir){
+		File folder=new File(dir.toString());
+		File[] rootFiles=folder.listFiles();
+		ArrayList<String> nonDuplicatingFiles=new ArrayList<String>();
+		
+		for(File rootFile : rootFiles){
+			if(rootFile.isDirectory()){
+				nonDuplicatingFiles.addAll(listDuplicatingFiles(rootFile));
+			}
+			else{
+				try {
+					if(files.get(getMD5Checksum(rootFile.toString()))==null){
+						files.put(getMD5Checksum(rootFile.toString()), rootFile.toString());
+						nonDuplicatingFiles.add(rootFile.getName().toString());
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return nonDuplicatingFiles;
+	}
+	
+	private static byte[] createChecksum(String filename) throws Exception {
+	       InputStream fis =  new FileInputStream(filename);
+
+	       byte[] buffer = new byte[1024];
+	       MessageDigest complete = MessageDigest.getInstance("MD5");
+	       int numRead;
+
+	       do {
+	           numRead = fis.read(buffer);
+	           if (numRead > 0) {
+	               complete.update(buffer, 0, numRead);
+	           }
+	       } while (numRead != -1);
+
+	       fis.close();
+	       return complete.digest();
+	   }
+	
+	public static String getMD5Checksum(String filename) throws Exception {
+	       byte[] b = createChecksum(filename);
+	       String result = "";
+
+	       for (int i=0; i < b.length; i++) {
+	           result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+	       }
+	       return result;
+	   }
+	
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		String path;
+		
+		System.out.println("Enter a full path to a directory: ");
+		path=in.nextLine();
+		
+		ArrayList<String> files=listDuplicatingFiles(new File(path));
+		for(String file : files){
+			System.out.println(file);
+		}
+		
+	}
+
+}
---
+++ Problem3/src/smallestSubstring.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class smallestSubstring {
+
+	static void smallestSubstringContainingTheAlphabet(String str) {
+		char[] chars; //we put the characters from the input string here
+		int[][] countOfCharacter = new int[str.length()][26]; //a table, containing the count of the letters of alphabet found in each substring
+		ArrayList<Integer> stringStart = new ArrayList<Integer>(); //contains the begin of each substring, containing the alphabet
+		ArrayList<Integer> stringLength = new ArrayList<Integer>(); //contains the length of each substring, containing the alphabet
+		int indexOfMin; //we put there the index of the substring which has min. length
+		int b; //the current character we check
+		int p = 0; //position. We use it for looping when we print the smallest found substring.
+		int charCount=0; //the count of the alphabetic characters we have found
+		boolean allLettersFound=false; //have we found all letters from the alphabet
+		
+		chars = str.toCharArray();
+		
+		for (int i = 0; i < chars.length; i++) {
+			for (int k = i; k < chars.length; k++) {
+				b = (int) chars[k];
+				charCount++;
+				/* checking if a character from the input is outside of ASCII */
+				if(b<0 || b>127){
+					System.err.println("Error! String contains symbols out of ASCII table.");
+					return;
+				}
+				/* checking if there is a blank space in the input string */
+				else if(b==32){
+					System.err.println("Error! String contains blank space.");
+					return;
+				}
+				/* checking if all of the letters in the alphabet is found at least
+				for at least one time */
+				else if (b >= 97 && b <= 122) {
+					countOfCharacter[i][b - 97]++;
+					/* we start checking if we have all characters from the alphabet after the 26-th character we've got.  */
+					if(charCount>=26){
+						for(int j=0; j<25; j++){
+							if(countOfCharacter[i][j]>0){
+								allLettersFound=true;
+							}else{
+								allLettersFound=false;
+								break;
+							}
+						}
+						if(allLettersFound==true){
+							stringStart.add(i);
+							stringLength.add(k - i);
+						}
+					}
+				}
+			}
+		}
+		
+		/*
+		 * for(int i=0; i<chars.length; i++){ for(int j=0; j<26; j++){
+		 * System.out.print(countOfCharacter[i][j] + " "); }
+		 * System.out.println(); }
+		 */
+		
+		/* Checks if there is a substring, containing the alphabet. 
+		 * If yes, then gets the index of the string which has the smallest length. 
+		 * We use it to find the begin of the string - they have same index.*/
+		if (stringLength.isEmpty()) {
+			System.out
+					.println("The input string doesn't contain the whole alphabet.");
+			return;
+		} else {
+			indexOfMin = getMin(stringLength);
+
+			/*
+			 * starts looping from the begin of the smallest substring until its
+			 * end.
+			 */
+			for (int i = stringStart.get(indexOfMin); p <= stringLength
+					.get(indexOfMin); i++, p++) {
+				System.out.print(chars[i]);
+			}
+			System.out.println();
+		}
+	}
+
+	/* We use it to find the begin of the string - they have same index.*/
+	public static int getMin(ArrayList<Integer> arr) {
+		int min = arr.get(0);
+		int i;
+		for (i = 0; i < arr.size(); i++) {
+			if (arr.get(i) < min) {
+				min = arr.get(i);
+			}
+		}
+		return arr.indexOf(min);
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String str;
+
+		System.out.println("Type a string: ");
+		str = in.nextLine();
+		smallestSubstring.smallestSubstringContainingTheAlphabet(str);
+
+		// smallestSubstring.smallestSubstringContainingTheAlphabet("aaaaaabcdefghijklmnopqrstuvwxyz");
+		// smallestSubstring.smallestSubstringContainingTheAlphabet("abcdefghijklmn124345678!@#$%^&*opqrstuvwxyz!*abcdefghijklmn");
+
+	}
+
+}
